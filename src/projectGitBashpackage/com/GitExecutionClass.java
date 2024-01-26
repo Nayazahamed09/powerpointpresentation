4202c2bd2d161015ed727cd0dd93155914605e0d
Nayazahamed@09 <nayazahamed.91307@gmail.com>
2024-01-26 12:03:41 +0530

    first commit

---
+++ src/projectGitBashpackage/com/GitExecutionClass.java
@@ -0,0 +1,13 @@
+package projectGitBashpackage.com;
+
+public class GitExecutionClass {
+	public static void main(String[] args)
+		{
+			System.out.println("Adding the data");
+		}
+	public static void getExtraction()
+	{
+		System.out.println("data is loading in the GitBash");
+	}
+	}
+
