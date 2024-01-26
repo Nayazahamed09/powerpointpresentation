bf58adf52fa4bb4befc7eb53516c40a95a5dd884
Nayazahamed@09 <nayazahamed.91307@gmail.com>
2024-01-26 12:25:30 +0530

    class has been added

---
+++ src/executionPackage/com/InstallPackage.java
@@ -0,0 +1,13 @@
+package executionPackage.com;
+
+public class InstallPackage {
+	public static void main(String[] args) {
+		
+		System.out.println("Hello java");
+	}
+	public static void installMethod()
+	{
+		System.out.println("Hello java packages are installed");
+	}
+
+}
